3a9adc044376b41c2f877b96e8557c4ec75dd1a1
HuangXingBo <hxb@apache.org>
2025-04-30 13:43:19 +0800

    [hotfix] upgrade ubuntu to ubuntu-22.04 in GHA (#54)

--- dev/build-wheels.sh
+++ dev/build-wheels.sh
@@ -22,31 +22,21 @@ PY_ENV_DIR=`pwd`/dev/.conda/envs
 py_env=("3.8" "3.9" "3.10" "3.11")
 ## 2. install dependency
 for ((i=0;i<${#py_env[@]};i++)) do
-    if [[ "$(uname)" != "Darwin" ]]; then
-        # force the linker to use the older glibc version in Linux
-        export CFLAGS="-I. -include dev/glibc_version_fix.h"
-    fi
     ${PY_ENV_DIR}/${py_env[i]}/bin/pip install -r dev/dev-requirements.txt
 done
 
 ## 3. build wheels
-for ((i=0;i<${#py_env[@]};i++)) do
-    ${PY_ENV_DIR}/${py_env[i]}/bin/python setup.py bdist_wheel
-done
-
-## 4. convert linux_x86_64 wheel to manylinux1 wheel in Linux
 if [[ "$(uname)" != "Darwin" ]]; then
-    source `pwd`/dev/.conda/bin/activate
-    # 4.1 install patchelf
-    conda install -c conda-forge patchelf=0.11 -y
-    # 4.2 install auditwheel
-    pip install auditwheel==3.2.0
-    # 4.3 convert Linux wheel
-    for wheel_file in dist/*.whl; do
-        auditwheel repair ${wheel_file} -w dist
-        rm -f ${wheel_file}
-    done
-    source deactivate
+    # force the linker to use the older glibc version in Linux
+    export CFLAGS="-I. -include dev/glibc_version_fix.h"
 fi
+
+for ((i=0;i<${#py_env[@]};i++)) do
+    if [[ "$(uname)" == "Darwin" ]]; then
+      "${PY_ENV_DIR}"/${py_env[i]}/bin/python setup.py bdist_wheel
+    else
+      "${PY_ENV_DIR}"/${py_env[i]}/bin/python setup.py bdist_wheel --plat-name manylinux1_x86_64
+    fi
+done
 ## see the result
 ls -al dist/
